24e23331a07ea4c77457983e6034cb964135dea4
DemonOverlord <mpire745@hotmail.com>
2021-11-02 21:06:44 +0530

    Added Colors and made some changes to the buttons

--- app/src/main/java/com/example/sudoku/MainActivity.kt
+++ app/src/main/java/com/example/sudoku/MainActivity.kt
@@ -6,10 +6,7 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,6 +14,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.sudoku.ui.theme.Orange
 import com.example.sudoku.ui.theme.SudokuTheme
 import kotlin.coroutines.coroutineContext
 
@@ -34,7 +33,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SudokuApp() {
-    Surface(modifier = Modifier.fillMaxSize(), color = Color.Cyan) {
+    Surface(modifier = Modifier.fillMaxSize(), color = Color.Black) {
         Buttons()
     }
 }
@@ -46,16 +45,16 @@ fun Greeting(name: String) {
 
 @Composable
 fun Buttons() {
-    var context = LocalContext.current;
+    val context = LocalContext.current;
     Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceEvenly) {
-        Button(onClick = { onClick("easy", context) }) {
-            Text(text = "Easy")
+        Button(onClick = { onClick("easy", context) }, colors = ButtonDefaults.buttonColors(Color.Blue)) {
+            Text(text = "Easy", fontSize = 30.sp, color = Color.White)
         }
-        Button(onClick = { onClick("medium", context)  }) {
-            Text(text = "Medium")
+        Button(onClick = { onClick("medium", context)  }, colors = ButtonDefaults.buttonColors(Orange)) {
+            Text(text = "Medium", fontSize = 30.sp, color = Color.White)
         }
-        Button(onClick = { onClick("hard", context)  }) {
-            Text(text = "Hard")
+        Button(onClick = { onClick("hard", context)  }, colors = ButtonDefaults.buttonColors(Color.Red)) {
+            Text(text = "Hard", fontSize = 30.sp, color = Color.White)
         }
     }
 }
