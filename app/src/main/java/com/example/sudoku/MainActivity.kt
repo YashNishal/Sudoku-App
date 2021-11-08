5c181c2447c7ea9aa00e8582b526e6785293199b
yash nishal <yashnishal@gmail.com>
2021-11-08 20:07:12 +0530

    added gradient

--- app/src/main/java/com/example/sudoku/MainActivity.kt
+++ app/src/main/java/com/example/sudoku/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -14,8 +15,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -48,7 +51,58 @@ fun SudokuApp() {
             Color1,
             Color2
         )))) {
+            val width = constraints.maxWidth*1.25f
+            val height = constraints.maxHeight/2f
 
+            // Medium colored path
+            val mediumColoredPoint1 = Offset(0f,height * 0.3f)
+            val mediumColoredPoint2 = Offset(width * 0.1f,height * 0.35f)
+            val mediumColoredPoint3 = Offset(width* 0.4f,height * 0.05f)
+            val mediumColoredPoint4 = Offset(width * 0.75f,height * 0.7f)
+            val mediumColoredPoint5 = Offset(width * 1.4f,-height.toFloat() )
+
+            val mediumColoredPath = Path().apply {
+                moveTo(mediumColoredPoint1.x,mediumColoredPoint1.y)
+                standardQuadFromTo(mediumColoredPoint1,mediumColoredPoint2)
+                standardQuadFromTo(mediumColoredPoint2,mediumColoredPoint3)
+                standardQuadFromTo(mediumColoredPoint3,mediumColoredPoint4)
+                standardQuadFromTo(mediumColoredPoint4,mediumColoredPoint5)
+                lineTo(width.toFloat() + 100f, height.toFloat()+300f)
+                lineTo(-100f, height.toFloat()+ 100f)
+                close()
+            }
+
+            // Light colored path
+            val lightPoint1 = Offset(0f, height * 0.35f)
+            val lightPoint2 = Offset(width * 0.1f, height * 0.4f)
+            val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
+            val lightPoint4 = Offset(width * 0.65f, height.toFloat())
+            val lightPoint5 = Offset(width * 1.4f, -height.toFloat() / 3f)
+
+            val lightColoredPath = Path().apply {
+                moveTo(lightPoint1.x, lightPoint1.y)
+                standardQuadFromTo(lightPoint1, lightPoint2)
+                standardQuadFromTo(lightPoint2, lightPoint3)
+                standardQuadFromTo(lightPoint3, lightPoint4)
+                standardQuadFromTo(lightPoint4, lightPoint5)
+                lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+                lineTo(-100f, height.toFloat() + 100f)
+                close()
+            }
+            Column {
+                Spacer(modifier = Modifier.fillMaxHeight(0.65f))
+                Canvas(
+                    modifier = Modifier.fillMaxSize()) {
+                    drawPath(
+                        path = mediumColoredPath,
+                        color = Curve,
+                    )
+                    drawPath(
+                        path = lightColoredPath,
+                        color = Curve
+                    )
+                }
+            }
         }
         Buttons()
     }
--- app/src/main/java/com/example/sudoku/ui/theme/Color.kt
+++ app/src/main/java/com/example/sudoku/ui/theme/Color.kt
@@ -11,12 +11,20 @@ val Purple = Color(0x6100b1ff)
 val TextWhite = Color(0xffeeeeee)
 val BrightBlue = Color(0xff2026AD)
 
-// Gradients
-val Color1 = Color(0xfff5f7fa)
-val Color2 = Color(0xffc3cfe2)
+// Gradients milk
+//val Color1 = Color(0xfff5efef)
+//val Color2 = Color(0xfffeada6)
+
+// Gradients aqua
+//val Color1 = Color(0xff13547a)
+//val Color2 = Color(0xff80d0c7)
+
+// Gradients mirror
+val Color1 = Color(0xff93a5cf)
+val Color2 = Color(0xffe4efe9)
 
 // Button Colors
 val Easy = Color(0xB741ACFF)
 
 // Curves
-val Curve = Color(0xC4FFFFFF)
+val Curve = Color(0xA4FFFFFF)
