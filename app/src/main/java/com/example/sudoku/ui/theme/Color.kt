bdc0b2cffea7b71633425665fb4ddf7a494aa2a1
DemonOverlord <mpire745@hotmail.com>
2021-11-13 15:29:52 +0530

    Added Icon and Removed some unused variables in Color.kt

--- app/src/main/java/com/example/sudoku/ui/theme/Color.kt
+++ app/src/main/java/com/example/sudoku/ui/theme/Color.kt
@@ -6,10 +6,7 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
-val Orange = Color(0xFFF58914)
-val Purple = Color(0x6100b1ff)
 val TextWhite = Color(0xffeeeeee)
-val BrightBlue = Color(0xff2026AD)
 
 // Gradients milk
 //val Color1 = Color(0xfff5efef)
@@ -20,20 +17,14 @@ val BrightBlue = Color(0xff2026AD)
 //val Color2 = Color(0xff80d0c7)
 
 // Gradients mirror
-val Color1 = Color(0xff93a5cf)
-val Color2 = Color(0xffe4efe9)
+//val Color1 = Color(0xff93a5cf)
+//val Color2 = Color(0xffe4efe9)
 
 // Dark
 val DColor3 = Color(0xff434343)
 val DColor1 = Color(0xff29323c)
 val DColor2 = Color(0xff485563)
 val DColor0 = Color(0xff000000)
-
-// Button Colors
-val Easy = Color(0xff81B214)
-val Medium = Color(0xFFFFB028)
-val Hard = Color(0xFF991515)
-val Random = Color(0xff41ACFF)
 
 // Curves
 val Curve = Color(0x7AFFFFFF)
